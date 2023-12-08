f972627803075213b35f395c3c923fb8a864a76c
NupeCoder <NupeCoder@gmail.com>
2023-12-08 22:48:35 +0100

    Built the responsive lyout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { Button } from '@chakra-ui/react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 
 const App = () => {
-  return;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      {' '}
+      <GridItem area="nav" bg="tomato">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg={'papayawhip'}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="gold">
+        Main
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default App;
