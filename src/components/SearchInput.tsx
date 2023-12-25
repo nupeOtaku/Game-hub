a0d72312a2ad1590d6dc9747748915222ec552a2
NupeCoder <NupeCoder@gmail.com>
2023-12-25 23:36:12 +0100

    Fixed the Navigation bar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,16 +2,19 @@ import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
 import { useRef } from 'react';
 import { BsSearch } from 'react-icons/bs';
 import useGamesQueryStore from '../store';
+import { useNavigate } from 'react-router-dom';
 
 const SearchInput = () => {
   const setSearchText = useGamesQueryStore((s) => s.setSearchText);
   const ref = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
     if (ref.current) {
       setSearchText(ref.current.value);
+      navigate('/');
     }
   };
 
