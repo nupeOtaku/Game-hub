f7e1ee2b2ef5e3d154fb9e08e879e9374066d71c
NupeCoder <NupeCoder@gmail.com>
2023-12-25 04:06:28 +0100

    Refactor the project to start using react-router-dom

---
+++ src/pages/GameDetailPage.tsx
@@ -0,0 +1,9 @@
+const GameDetailPage = () => {
+  return (
+    <div>
+      <h1>Game Detail Page</h1>
+    </div>
+  );
+};
+
+export default GameDetailPage;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,39 @@
+import { Grid, Show, GridItem, HStack, Box } from '@chakra-ui/react';
+import GameGrid from '../components/GameGrid';
+import GenreList from '../components/GenreList';
+import PlatformSelector from '../components/PlatformSelector';
+import SortSelector from '../components/SortSelector';
+import GameHeading from '../components/GameHeading';
+
+const HomePage = () => {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"main"`,
+        lg: `"aside main"`,
+      }}
+      templateColumns={{
+        base: '1fr',
+        lg: '200px 1fr',
+      }}
+    >
+      <Show above="lg">
+        <GridItem area="aside" paddingX={5}>
+          <GenreList />
+        </GridItem>
+      </Show>
+      <GridItem area="main">
+        <Box paddingX={2}>
+          <GameHeading />
+          <HStack spacing={5} marginBottom={5}>
+            <PlatformSelector />
+            <SortSelector />
+          </HStack>
+        </Box>
+        <GameGrid />
+      </GridItem>
+    </Grid>
+  );
+};
+
+export default HomePage;
